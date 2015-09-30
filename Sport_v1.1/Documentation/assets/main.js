ee68a2a7c71c75fcbce4a1352790b641f13d66f5
robotcigan <robotcigan@gmail.com>
2015-09-30 09:30:07 +0300

    first commit

---
+++ Sport_v1.1/Documentation/assets/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+ 
+ // initiate page scroller plugin
+ $('body').pageScroller({
+ 	navigation: '#nav'
+ });
+ 
+
+ // Fancybox.js
+	 $('.fancybox').fancybox();
+	  
+	 
+  // Mosaic.js		 
+	 $('.fade').mosaic();
+	 
+	 
+});
+
+
